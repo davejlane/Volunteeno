c4d48e432ec6fcdb93f5ad2eb5638acf10626673
Dave Lane <dlane@whitepages.com>
2014-02-05 08:26:23 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Volunteeno::Application.config.secret_key_base = '79f6578dce40ded59c1b217e8a022eae61302f9195b2376579062470f0d74763494744029fac178511165408fd747270b262a0996d9cc850495b2bb4451b385f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Volunteeno::Application.config.session_store :cookie_store, key: '_Volunteeno_session'
