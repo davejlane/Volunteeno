8ee8197d2c68a62773a4309919c63264abbcefc9
Mahdi Saeed <mahadi@gmail.com>
2014-02-17 08:59:56 -0800

    Adding validation to name and categories to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Volunteeno::Application.routes.draw do
 
   root :to => "home#index"
+
+
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
