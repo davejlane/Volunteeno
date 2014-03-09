e7ead8353007265f2f8e310e3b43ef5db8404f83
Mahdi Saeed <mahadi@gmail.com>
2014-03-09 15:21:46 -0700

    Adding folders that I deleted by mistake

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Volunteeno::Application.config.secret_key_base = '769fe439de86dbb5215d3c361799a056addb91b676b3d3744df7f2eb11d0151fc820940610ca3d5cb92205c776b0c5a74d3d04a8d7537d232985f0218ca5088e'
